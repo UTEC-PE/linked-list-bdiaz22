8bbe064880f73d1172caa405edb321a9df369758
bdiaz22 <33464840+bdiaz22@users.noreply.github.com>
2018-08-21 21:58:08 -0500

    avance de la lista
    avance de push_front y de push_back

--- list.h
+++ list.h
@@ -17,21 +17,94 @@ class List {
         void print_reverse(Node<T>* head);
 
     public:
-        List();
+        List(){
+            head = NULL;
+            tail = NULL;
+            nodes = 0; 
+        }
 
-        int front();
-        int back();
-        void push_front(T value);
-        void push_back(T value);
-        void pop_front();
-        void pop_back();
-        T get(int position);
+        T front()
+        {
+            return head -> data;
+        }
+        T back()
+        {
+            return tail -> data;
+        }
+        void push_front(T value)
+        {
+            if(!head)
+            {
+                head = new Node<T>;
+                head->data = value;
+                head->next = NULL;
+                tail=head;
+            }
+            else
+            {
+                Node<T> *temp = new Node<T>;
+                temp->data = value;
+                temp->next = head;
+                head = temp;
+            }
+            nodes++;
+        }
+        void push_back(T value)
+        {
+            if(!head)
+            {
+                head = new Node<T>;
+                tail->data = value;
+                tail->next = NUll;
+                head = tail;
+            }
+            else
+            {
+                Node<T> *temp =  new Node<T>;
+                temp->data = value;
+                temp->next = NUll;
+                tail->next= temp;
+                tail = temp;
+            }
+            nodes++;
+        }
+        void pop_front()
+        {
+
+        }
+        void pop_back()
+        {
+
+        }
+        T get(int position)
+        {
+
+        }
         void concat(List<T> &other);
-        bool empty();
-        int size();
-        void print();
-        void print_reverse();
-        void clear();
+        bool empty()
+        {
+
+        }
+        int size()
+        {
+
+        }
+        void print()
+        {
+            Node<T> *temp = head;
+            if(temp != NUll)
+            {
+                
+            }
+        }
+        void print_reverse()
+        {
+
+        }
+        void clear()
+        {
+
+        }
         Iterator<T> begin();
         Iterator<T> end();
 
